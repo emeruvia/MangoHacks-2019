d637f8cce55cefb40e47f42f12178550ffdaa3df
Edgar Meruvia <emeruvia@gmail.com>
2019-02-02 12:07:51 -0500

    Add code styleguide

--- android-app/app/src/main/java/fgcu/mangohacks2019/models/User.kt
+++ android-app/app/src/main/java/fgcu/mangohacks2019/models/User.kt
@@ -1,7 +1,9 @@
 package fgcu.mangohacks2019.models
 
-data class User(var name: String,
-                     var image: String,
-                     var baseCity: String,
-                     var email:String,
-                     var password: String) {}
+data class User(
+  var name: String,
+  var image: String,
+  var baseCity: String,
+  var email: String,
+  var password: String
+)
