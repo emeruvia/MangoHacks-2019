16bb61401846eb1f3ae6903f9a2def04ffca6989
jfleurent <jeffreyfleurent@outlook.com>
2019-02-02 06:39:34 -0500

    Made connection to the login and signup page

--- android-app/app/src/main/java/fgcu/mangohacks2019/SignUpActivity.kt
+++ android-app/app/src/main/java/fgcu/mangohacks2019/SignUpActivity.kt
@@ -1,12 +1,27 @@
 package fgcu.mangohacks2019
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class SignUpActivity : AppCompatActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_sign_up)
+  }
+
+  fun onClick(view: View){
+    when(view.id){
+      R.id.login_textview -> {
+        val intent = Intent(this,LoginActivity::class.java)
+        startActivity(intent)
+      }
+      R.id.sign_up_button -> {
+        val intent = Intent(this,LoginActivity::class.java)
+        startActivity(intent)
+      }
+    }
   }
 }
