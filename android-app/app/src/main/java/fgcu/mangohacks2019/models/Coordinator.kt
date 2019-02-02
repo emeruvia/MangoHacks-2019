56a0c2a8df29cc32e79624a2044b44b4d3c73107
Juan Gomez <juanangelgomez@live.com>
2019-02-02 03:41:57 -0500

    model class added

---
+++ android-app/app/src/main/java/fgcu/mangohacks2019/models/Coordinator.kt
@@ -0,0 +1,8 @@
+package fgcu.mangohacks2019.models
+
+data class Coordinator (var name: String,
+                        var image: String,
+                        var baseCity: String,
+                        var email:String,
+                        var password: String,
+                        var phoneNumber: String) {}
---
+++ android-app/app/src/main/java/fgcu/mangohacks2019/models/Event.kt
@@ -0,0 +1,12 @@
+package fgcu.mangohacks2019.models
+
+import java.util.*
+
+data class Event (var name: String,
+                        var address: String,
+                        var coordintator: Coordinator,
+                        var image:String,
+                        var description: String,
+                        var date: Date,
+                        var price: Double
+                  ) {}
