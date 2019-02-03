5b5ee1e6aeb503b51af7c1496d4f9f266d15e348
Juan Gomez <juanangelgomez@live.com>
2019-02-03 03:51:18 -0500

    implemention of lottie to login page

--- android-app/app/src/main/java/fgcu/mangohacks2019/LoginActivity.kt
+++ android-app/app/src/main/java/fgcu/mangohacks2019/LoginActivity.kt
@@ -4,12 +4,19 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import com.airbnb.lottie.LottieAnimationView
+
 
 class LoginActivity : AppCompatActivity() {
+
+  lateinit var lottieIv: LottieAnimationView
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_login)
+
+    lottieIv = findViewById(R.id.world)
+
   }
 
   fun onClick(view: View){
